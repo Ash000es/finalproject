b07b2334b6f2c98b1d361a0f94037a37d1181aef
ash000es <ash007.uk@icloud.com>
2020-03-13 23:01:05 +0100

    aa

--- src/components/HotelList/HoteList.js
+++ src/components/HotelList/HoteList.js
@@ -2,12 +2,14 @@ import React from 'react'
 import '../HotelList/HotelList.css'
 
 import Hotel from '../Hotel/Hotel'
+import Extras from '../Extras/Extras'
 
 class HotelList extends React.Component {
   render () {
     return (
       <div className='HotelList'>
         <Hotel />
+        <Extras />
         <Hotel />
         <Hotel />
         <Hotel />
