b5403eed86dbf2e848d112860506cb69f8f7d510
ash000es <ash007.uk@icloud.com>
2020-03-15 22:21:16 +0100

    aa

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,9 @@ import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import { LinkContainer } from 'react-router-bootstrap'
+import CardDeck from 'react-bootstrap/CardDeck'
+import Card from 'react-bootstrap/Card'
+
 import '../App/App.css'
 import Hotel from '../Hotel/Hotel'
 import hotel from '../assets/hotel.png'
@@ -31,93 +34,107 @@ const hotelData = {
 }
 const extras = {
   extrasPrice: { breakfast: 10, cake: 20, redwine: 20 },
-  extrasImages: { breakfast:'', cake: '', redwine:'' }
-  extrasSummary:{breakfast:'Kings breakfast for served in bed', cake:' Double chocolate cake with strawberries', redwine:'A bottle of our berry sweet house red'},
-  extrasDescription: {breakfast:' Kings Breakfast is served in bed and includes: orange juice, pastry, posh eggs, a selection of cheeses, coffee', cake:'We call it death by chocolate, two layers of dark and white chocolate infusion will send you to seven heavens', redwine:'our !995 red merlot is famous for its sweet berry tasts, ideal to accompany chicken or fish dishes'}
+  extrasImages: { breakfast: '', cake: '', redwine: '' },
+  extrasSummary: { breakfast: 'Kings breakfast for served in bed', cake: ' Double chocolate cake with strawberries', redwine: 'A bottle of our berry sweet house red' },
+  extrasDescription: { breakfast: ' Kings Breakfast is served in bed and includes: orange juice, pastry, posh eggs, a selection of cheeses, coffee', cake: 'We call it death by chocolate, two layers of dark and white chocolate infusion will send you to seven heavens', redwine: 'our !995 red merlot is famous for its sweet berry tasts, ideal to accompany chicken or fish dishes' }
 
 }
 
 function App () {
-  return (
-    <MemoryRouter>
-      <Navbar id='nav' expand='sm' bg='dark' variant='dark'>
-        <Navbar.Brand>Website</Navbar.Brand>
-        <Nav className='mr-auto'>
-          <LinkContainer to='/home'>
-            <Nav.Link>Home</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to='/users'>
-            <Nav.Link>Users</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to='/about'>
-            <Nav.Link>About</Nav.Link>
-          </LinkContainer>
-        </Nav>
-      </Navbar>
-      <Container className='pt-3'>
-        <Switch>
-          <Route path='/home'>
-            {/* <Home /> */}
-          </Route>
-          <Route path='/about'>
-            {/* <About /> */}
-          </Route>
-          <Route path='/users'>
-            {/* <Users /> */}
-          </Route>
-        </Switch>
-        {/* <HotelPage /> */}
-        <div>
-          <img/>
-          <SearcBar />
+  /* 1.fetch function here for top destination for defualte display
+  2.Handle card click function here which land them on the destination search results.
+  3.create onClick funtion here which return the <hoteList/>
 
-        </div>
-        
-        <CardDeck>
-          <Card>
-            <Card.Img variant='top' src='holder.js/100px160' />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className='text-muted'>Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant='top' src='holder.js/100px160' />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This card has supporting text below as a natural lead-in to additional
-                content.{' '}
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className='text-muted'>Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant='top' src='holder.js/100px160' />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This card has even longer content than the first to
-                show that equal height action.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className='text-muted'>Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-        </CardDeck>
-        <Hotel hotel={hotelData} />
-      </Container>
-    </MemoryRouter>
+on click Inspire me button function to render <popUpSlider/> with the slider components
+funtion to capture the sliders input and pass it to the recommmendation algorithm.
+ the return results is another components called <inspiremeResults/>
+ <inspiremeResults/> component= 3 row destination card with 3 cards on top
+
+  */
+
+  return (
+    <>
+      <MemoryRouter>
+        <Navbar id='nav' expand='sm' bg='dark' variant='dark'>
+          <Navbar.Brand>Website</Navbar.Brand>
+          <Nav className='mr-auto'>
+            <LinkContainer to='/home'>
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/users'>
+              <Nav.Link>Users</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/about'>
+              <Nav.Link>About</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar>
+        <Container className='pt-3'>
+          <Switch>
+            <Route path='/home'>
+              {/* <Home /> */}
+            </Route>
+            <Route path='/about'>
+              {/* <About /> */}
+            </Route>
+            <Route path='/users'>
+              {/* <Users /> */}
+            </Route>
+          </Switch>
+          {/* <HotelPage /> */}
+
+          <div>
+            <img />
+            <SearchBar />
+
+          </div>
+          <p>Popular destinations</p>
+
+          <CardDeck>
+            <Card>
+              <Card.Img variant='top' src='holder.js/100px160' />
+              <Card.Body>
+                <Card.Title>Madrid</Card.Title>
+                <Card.Text>
+                  Hotels from 22$
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className='text-muted'>Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img variant='top' src='holder.js/100px160' />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This card has supporting text below as a natural lead-in to additional
+                  content.{' '}
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className='text-muted'>Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img variant='top' src='holder.js/100px160' />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This card has even longer content than the first to
+                  show that equal height action.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className='text-muted'>Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+          </CardDeck>
+          {/* <Hotel hotel={hotelData} /> */}
+        </Container>
+      </MemoryRouter>
+    </>
 
   )
 }
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,19 +1,44 @@
 import React from 'react'
+import DatePicker from '../DatePicker/DatePicker'
+import Sign from 'js-sha256'
 
-class SearchBar extends Component {
-    state = {  }
-    render() { 
-        return (  
-            <div className='SearchBar-fields'>
+class SearchBar extends React.Component {
+    state = { }
+    handleClickButton= () => {
+      const apikey = '2t97t6954dckh4ynkwknr78j'
+      const sec = 'nDD9BFXf5a'
+      const D = new Date()
+
+      const getSignature = () => {
+        return Sign(apikey + sec + 1584307101)
+      }
+      console.log(getSignature())
+      fetch(' https://cors-anywhere.herokuapp.com/https://api.test.hotelbeds.com/hotel-content-api/1.0/hotels?fields=all&language=ENG&from=1&to=100&useSecondaryLanguage=false',
+        {
+          headers: {
+            'Api-key': apikey,
+            'X-Signature': getSignature(),
+            Accept: 'application/json',
+            'Accept-Encoding': 'gzip'
+          }
+        })
+    }
+
+    render () {
+      return (
+        <>
+          <div className='SearchBar-fields'>
             <input placeholder='Where?' />
-            <input placeholder='When?' />
+            {/* <search/> */}
+            {/* <DatePicker /> */}
           </div>
           <div className='SearchBar-submit'>
-            <a >Let's Go</a>
+            <button onClick={this.handleClickButton}>Search</button>
+            <button>Inspire me</button>
           </div>
-
-        );
+        </>
+      )
     }
 }
- 
-export default SearchBar;
+
+export default SearchBar
