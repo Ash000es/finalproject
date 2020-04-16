b8864fbde0ab2a754f58e8779191d40dcf1345f0
ash000es <ash007.uk@icloud.com>
2020-04-16 16:46:36 +0200

    a

--- src/ config-overrides.js
+++
@@ -1,8 +0,0 @@
-const { override, addLessLoader } = require('customize-cra')
-
-module.exports = override(
-  addLessLoader({
-    javascriptEnabled: true,
-    modifyVars: { '@base-color': '#f44336' }
-  })
-)
