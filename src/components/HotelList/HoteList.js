a641450d5f671dafcd1ec297fd8eaf22d9b13be9
ash000es <ash007.uk@icloud.com>
2020-04-21 22:29:27 +0200

    a

--- src/components/HotelList/HoteList.js
+++ src/components/HotelList/HoteList.js
@@ -5,6 +5,7 @@ import { DropDownFilter } from '../DropDownFilter/DropDownFilter'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import { hardHotelObject } from '../assets/HardCode'
+import './HotelList.css'
 
 class HotelList extends React.Component {
   render () {
@@ -12,12 +13,18 @@ class HotelList extends React.Component {
       <div className='HotelList'>
         <SearchResultsHero />
         <Typography>{hardHotelObject.destinationName}</Typography>
-        <DropDownFilter />
-        <DetailedExpansionPanel />
-        <DetailedExpansionPanel />
-        <DetailedExpansionPanel />
-        <DetailedExpansionPanel />
-        <DetailedExpansionPanel />
+        <div className='sortButton'>
+          <DropDownFilter />
+        </div>
+        <br />
+        <br />
+        <div className='list'>
+          <DetailedExpansionPanel />
+          <DetailedExpansionPanel />
+          <DetailedExpansionPanel />
+          <DetailedExpansionPanel />
+          <DetailedExpansionPanel />
+        </div>
 
       </div>
     )
