88ef7981b144b28ffb6931f8e2c9aa6e83b06e81
ash000es <ash007.uk@icloud.com>
2020-04-25 11:21:57 +0200

    a

--- src/components/Hotelpage/HotelPage.js
+++ src/components/Hotelpage/HotelPage.js
@@ -12,25 +12,25 @@ import DateRangePicker from '../DateRange/NewDateRange'
 // on click we need content api call to pull the hotel information ( des+img)
 // we should have made the price and availability call before in search results so click on book will take you to the hotel page
 const HotelPage = (props) => {
-  const cancelationDate = hardHotelObject.rooms[0].rates[0].cancellationPolicies[0].from
+  const cancelationDate = props.hotel.rooms[0].rates[0].cancellationPolicies[0].from
   return (
     <>
       <h4>This hotel have spceial offer for you</h4>
-      <p>{props.name}</p>
+      <p>{props.hotel.name}</p>
       <p>{hardHotelObject.label.posh}</p>
-      <p>{props.address},
-        {props.postalCode},
-        {props.city} show on map
+      <p>{props.hotel.address},
+        {props.hotel.postalCode},
+        {props.hotel.city} show on map
       </p>
-      <p>{props.rooms[0].rates[0].net}</p>
+      <p>{props.hotel.rooms[0].rates[0].net}</p>
       <p>Edit your dates</p>
 
       <DateRangePicker />
       <div>
         <ControlledCarousel />
       </div>
-      <div>amenities:
-        <p>{props.Numberofrooms.number} Rooms</p>
+      <div>amenities:bn
+        <p>{props.hotel.Numberofrooms.number} Rooms</p>
         <ul style={{ listStyleType: 'none' }}>{hardHotelObject.hotelAmenities.map(item =>
           <li key={item.title}>{item.Icon}{item.title}</li>
         )}
@@ -58,22 +58,22 @@ const HotelPage = (props) => {
         <tbody>
           <tr>
             <td>{
-              props.rooms[0].name
+              props.hotel.rooms[0].name
             }
               <ControlledCarousel1 />
 
             </td>
-            <td>Adults {props.rooms[0].rates[0].adults}
-              Children {props.rooms[0].rates[0].children}
+            <td>Adults {props.hotel.rooms[0].rates[0].adults}
+              Children {props.hotel.rooms[0].rates[0].children}
             </td>
             <td>Free Wifi</td>
-            <td>{props.rooms[0].rates[0].net}$<Button variant='primary'>Book now</Button>{' '}Free cancelation till{cancelationDate}</td>
+            <td>{props.hotel.rooms[0].rates[0].net}$<Button variant='primary'>Book now</Button>{' '}Free cancelation till{cancelationDate}</td>
           </tr>
 
         </tbody>
         <tbody>
           <p>Your booknig from {} to {}</p>
-          <p>{props.rooms[0].rates[0].net}</p>
+          <p>{props.hotel.rooms[0].rates[0].net}</p>
           <Button variant='primary'>Book now</Button>{' '}
           <Button variant='outline-primary'>Add to cart</Button>{' '}
 
