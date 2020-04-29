99785d7221d5bbcfb05d3860dcb4d315a12aad30
ash000es <ash007.uk@icloud.com>
2020-04-29 22:04:41 +0200

    a

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,6 +2,8 @@ import React from 'react'
 import SearchBar from '../SearchBar/SearchBar'
 import HomePageResults from '../HomePageResults/HomepageResults'
 import Hero from '../Hero/Hero'
+import { MyProvider } from '../Provider/Provider'
+const MyContext = React.createContext()
 
 export const HomePage = () => {
   return (
