b4043d00636b6b21866530af51075e4bdf11aaba
ash000es <ash007.uk@icloud.com>
2020-05-18 22:53:17 +0200

    bracket correction

--- src/components/Hotelpage/HotelPage.js
+++ src/components/Hotelpage/HotelPage.js
@@ -26,78 +26,74 @@ const HotelPage = () => {
 
     <>
 
-      return (
-      <>
-        <h4>This hotel have spceial offer for you</h4>
-        <div> <p>{currentSelection.name}{hardHotelObject.label.posh}{currentSelection.address},{hardHotelObject.postalCode},{currentSelection.city} show on map</p></div>
+      <h4>This hotel have spceial offer for you</h4>
+      <div> <p>{currentSelection.name}{hardHotelObject.label.posh}{currentSelection.address},{hardHotelObject.postalCode},{currentSelection.city} show on map</p></div>
 
-        <div>
-          <p>Edit your dates</p>
-          <DateRangePicker />
-        </div>
+      <div>
+        <p>Edit your dates</p>
+        <DateRangePicker />
+      </div>
 
-        <HotelCarousel />
+      <HotelCarousel />
 
-        <div>amenities:bn
-          <p>70 Rooms</p>
-          <ul style={{ listStyleType: 'none' }}>{currentSelection.amenities2.map(item =>
-            <li key={item}>{item.icon}{item.title}</li>
-          )}
-          </ul>
+      <div>amenities:bn
+        <p>70 Rooms</p>
+        <ul style={{ listStyleType: 'none' }}>{currentSelection.amenities2.map(item =>
+          <li key={item}>{item.icon}{item.title}</li>
+        )}
+        </ul>
 
-        </div>
-        <img src='' alt='TA' />
-        <StarRatingDisplay />
-        <p>key facts:nothing</p>
-        <p>Check-in from
-          Check-out from
-        </p>
-        <p>{hardHotelObject.CheckinRules.title} required at Check-in</p>
-        <p>City tax will be collected by hotel at check-in or check-out.</p>
-        {/* function to map rooms here to return many rooms in the table, also where extras will go here? */}
-        <Table striped bordered hover size='sm'>
-          <thead>
-            <tr>
-              <th>Room Type</th>
-              <th>Adults</th>
-              <th>Included</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
+      </div>
+      <img src='' alt='TA' />
+      <StarRatingDisplay />
+      <p>key facts:nothing</p>
+      <p>Check-in from
+        Check-out from
+      </p>
+      <p>{hardHotelObject.CheckinRules.title} required at Check-in</p>
+      <p>City tax will be collected by hotel at check-in or check-out.</p>
+      {/* function to map rooms here to return many rooms in the table, also where extras will go here? */}
+      <Table striped bordered hover size='sm'>
+        <thead>
+          <tr>
+            <th>Room Type</th>
+            <th>Adults</th>
+            <th>Included</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
 
-            {currentSelection.rooms.map((room, i) => {
-              return (
+          {currentSelection.rooms.map((room, i) => {
+            return (
 
-                <tr key={room[i]} room={room[i]}>
+              <tr key={room[i]} room={room[i]}>
 
-                  <td>
-                    {/* <RoomCarousel room={room[i]} /> */}
-                    {/* passing the room object here is ok? */}
-                    <p>{room.name[i]}</p>
+                <td>
+                  {/* <RoomCarousel room={room[i]} /> */}
+                  {/* passing the room object here is ok? */}
+                  <p>{room.name[i]}</p>
 
-                  </td>
-                  <td>{room.rates.map((rate, i) => rate.adults)}</td>
+                </td>
+                <td>{room.rates.map((rate, i) => rate.adults)}</td>
 
-                  <td>Function to display only room amentities based on group facilites code</td>
-                  {/* <td><p>{room.rates.map(rate => rate.net)}</p><Button variant='primary'>Book now</Button>{' '}<p>{showCancelationPolicy(room)}</p></td> */}
-                </tr>
+                <td>Function to display only room amentities based on group facilites code</td>
+                {/* <td><p>{room.rates.map(rate => rate.net)}</p><Button variant='primary'>Book now</Button>{' '}<p>{showCancelationPolicy(room)}</p></td> */}
+              </tr>
 
-              )
-            })}
+            )
+          })}
 
-          </tbody>
-          <tbody>
-            <p>Your booknig from {} to {}</p>
-            <p>130</p>
-            <Button variant='primary'>Book now</Button>{' '}
-            <Button variant='outline-primary'>Add to cart</Button>{' '}
-            <CartDrawer />
+        </tbody>
+        <tbody>
+          <p>Your booknig from {} to {}</p>
+          <p>130</p>
+          <Button variant='primary'>Book now</Button>{' '}
+          <Button variant='outline-primary'>Add to cart</Button>{' '}
+          <CartDrawer />
 
-          </tbody>
-        </Table>
-      </>
-      )
+        </tbody>
+      </Table>
 
     </>
   )
