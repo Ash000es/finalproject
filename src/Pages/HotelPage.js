cb7fe79b5b121bf00fda773c4de29d51a0b50b24
ash000es <ash007.uk@icloud.com>
2020-08-21 21:49:47 +0200

    start rating dipaly is done in hotelpage

--- src/Pages/HotelPage.js
+++ src/Pages/HotelPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useMemo } from 'react'
 import { hardHotelObject } from '../assets/HardCode'
 import RoomCarousel from '../components/thumbilCarousel'
-import StarRatingDisplay from '../components/StarRatingDisplay'
+
 import Button from 'react-bootstrap/Button'
 import DateRangePicker from '../components/DateRange/NewDateRange'
 import { SearchResultsCarousel } from '../components/SearchResultsCarousel'
@@ -14,6 +14,7 @@ import { hotelAmen } from '../Helper/SearchResultsAmen'
 import { masterLinkLarge } from '../Helper/Constants'
 import './HotelPage.css'
 import CustomizedRatings from '../components/TripAdvisorRating'
+import StarRatings from '../components/StarRatings'
 
 const HotelPage = React.memo((props) => {
   const { project, setProject } = useContext(ProjectContext)
@@ -122,7 +123,7 @@ const HotelPage = React.memo((props) => {
           </div>
           <div className='starRating-Reviews'>
 
-            <StarRatingDisplay currentSelection={currentSelection} />
+            <StarRatings currentSelection={currentSelection} />
 
           </div>
         </div>
---
+++ src/components/StarRatings.js
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react'
+
+import { withStyles } from '@material-ui/core/styles'
+import Rating from '@material-ui/lab/Rating'
+import StarBorderIcon from '@material-ui/icons/StarBorder'
+
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+
+export const StarRating = withStyles({
+  iconFilled: {
+    color: '#ff6d75'
+  },
+  iconHover: {
+    color: '#ff3d47'
+  }
+})(Rating)
+
+export default function StarRatings (props) {
+  let starsRatings
+  const displayStarCount = (props) => {
+    console.log(props, 'props here ')
+    if (props.currentSelection.categoryName === '3 STARS') {
+      starsRatings = { starsName: '3 Stars hotel', starNumber: 3 }
+      return starsRatings
+    } if (props.currentSelection.categoryName === '4 STARS') {
+      starsRatings = { starsName: '4 Stars hotel', starNumber: 4 }
+      return starsRatings
+    } else if (props.currentSelection.categoryName === '5 STARS') {
+      starsRatings = { starsName: '5 Stars hotel', starNumber: 5 }
+      return starsRatings
+    } else {
+      starsRatings = { starsName: 'Not Rated', starNumber: 0 }
+      return starsRatings
+    }
+  }
+  //   useEffect(() => {
+  //     displayStarCount(props)
+  //   }, [])
+  displayStarCount(props)
+  console.log(starsRatings, 'staryyy')
+  return (
+    <div>
+      <Box component='fieldset' mb={3} borderColor='transparent'>
+        <Typography component='legend'>{starsRatings.starsName}</Typography>
+        <Rating
+          name='customized-empty'
+          defaultValue={starsRatings.starNumber}
+          precision={0.5}
+          emptyIcon={<StarBorderIcon fontSize='inherit' />}
+        />
+      </Box>
+    </div>
+  )
+}
